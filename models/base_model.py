3e3d6980e8b0033f05a8c47ce36ac94e2781252f
foash-111 <mr2221@fayoum.edu.eg>
2024-02-14 15:38:25 +0200

    add **kwargs option

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ from datetime import datetime
 class BaseModel:
     """main class for this projecta
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """constructor
 
         Args:
@@ -17,9 +17,17 @@ class BaseModel:
             created_at (_type_, str): time obj created. Defaults to None.
             updated_at (_type_, str): time obj updated. Defaults to None.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            self.id = kwargs.get('id')
+            self.created_at = datetime.fromisoformat(kwargs.get('created_at'))
+            self.updated_at = datetime.fromisoformat(kwargs.get('updated_at'))
+            for key, value in kwargs.items():
+                if key not in self.__dict__.keys():
+                    self.__dict__[key] = value
 
     def __str__(self):
         """string representation of an object"""
