8b7a6c84c98be4f248792a2fc535134fa35917cb
foash-111 <mr2221@fayoum.edu.eg>
2024-02-14 20:45:33 +0200

    add file storage

--- foash_test/models/base_model.py
+++ foash_test/models/base_model.py
@@ -4,6 +4,7 @@
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -21,10 +22,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
         else:
             self.id = kwargs.get('id')
             self.created_at = datetime.fromisoformat(kwargs.get('created_at'))
             self.updated_at = datetime.fromisoformat(kwargs.get('updated_at'))
+            for key, value in kwargs.items():
+                if key not in self.__dict__.keys():
+                    self.__dict__[key] = value
 
     def __str__(self):
         """string representation of an object"""
@@ -34,6 +39,7 @@ class BaseModel:
         """updates the public instance attribute
         updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all
--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -21,6 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
         else:
             self.id = kwargs.get('id')
             self.created_at = datetime.fromisoformat(kwargs.get('created_at'))
@@ -37,6 +39,7 @@ class BaseModel:
         """updates the public instance attribute
         updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all
