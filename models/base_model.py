f4ade6da08658c88b03ffaf8fcaa9bb004cabd07
foash-111 <mr2221@fayoum.edu.eg>
2024-02-14 13:06:47 +0200

    edit  base_model class and add some unittest checks for it

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """main class for this projecta
     """
@@ -25,14 +26,16 @@ class BaseModel:
         return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
+        """updates the public instance attribute
+        updated_at with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance
+        """returns a dictionary containing all
+        keys/values of __dict__ of the instance
         """
         new_dict = self.__dict__
         new_dict[__class__] = __class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
-        return(new_dict)
+        return (new_dict)
