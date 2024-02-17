f5428037fbfe9e8b127167091d1e63d5243a21ee
shaimaa <shaimafikry@yahoo.com>
2024-02-17 20:55:06 +0200

    adding tests

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -11,34 +11,36 @@ import os
 
 class testFileStorage(unittest.TestCase):
     """class for test basemodel methods"""
+    def setUp(self):
+        self.storage = FileStorage()
 
     def test_all(self):
         """test all function"""
         inst1 = BaseModel()
-        test = FileStorage.all(self)
+        test = self.storage.all()
         self.assertIsInstance(test, dict)
         self.assertIn(inst1, test.values())
 
     def test_new(self):
         """test new"""
         inst1 = BaseModel()
-        FileStorage.new(self, inst1)
-        test = FileStorage.all(self)
+        self.storage.new(inst1)
+        test = self.storage.all()
         self.assertIn("BaseModel." + (inst1.id), test)
 
     def test_save(self):
-        """test new"""
+        """test save"""
         inst1 = BaseModel()
-        FileStorage.new(self, inst1)
-        FileStorage.save(self)
+        self.storage.new(inst1)
+        self.storage.save()
         file_check = os.path.exists("Airbnb.json")
         self.assertEqual(True, file_check)
 
     def test_reload(self):
         """test reload from file"""
         inst1 = BaseModel()
-        FileStorage.new(self, inst1)
-        FileStorage.save(self)
-        FileStorage.reload(self)
-        test = FileStorage.all(self)
+        self.storage.new(inst1)
+        self.storage.save()
+        self.storage.reload()
+        test = self.storage.all()
         self.assertIn("BaseModel." + (inst1.id), test)
